d29c957082e8e88eef05945c52b0fe99875c035c
Patissonchik <bokov.es@mail.ru>
2020-02-10 17:54:24 +0500

    clown's adventure on androidplace

--- app/src/main/java/org/itcube/norsemythology/MainActivity.java
+++ app/src/main/java/org/itcube/norsemythology/MainActivity.java
@@ -2,6 +2,10 @@ package org.itcube.norsemythology;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("onClick", "RIBESHKA");
+            }
+        };
+
+        ListView listView = findViewById(R.id.list);
+        listView.setOnItemClickListener(listener);
     }
 }
